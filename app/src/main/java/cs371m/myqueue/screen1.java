5fa651e2ad4550147f431c899967c5e05bbbbf04
Erin Jensby <erinjensby@wireless-10-147-92-161.public.utexas.edu>
2017-04-03 12:59:24 -0500

    added Toolbar to screen1

--- app/src/main/java/cs371m/myqueue/screen1.java
+++ app/src/main/java/cs371m/myqueue/screen1.java
@@ -1,23 +1,14 @@
 package cs371m.myqueue;
 
-import android.app.Activity;
-import android.app.Fragment;
-import android.app.FragmentTransaction;
 import android.app.ListFragment;
 import android.content.Intent;
-import android.content.res.Configuration;
 import android.os.Bundle;
-import android.support.v4.content.ContextCompat;
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.MotionEvent;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
-import android.widget.TextView;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -27,7 +18,7 @@ import java.util.List;
  * This sample provides a different layout (and activity flow) when run in
  * landscape.
  */
-public class screen1 extends Activity {
+public class screen1 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,7 +34,10 @@ public class screen1 extends Activity {
             }
         });
 
-    }
+        Toolbar screen1Toolbar = (Toolbar)findViewById(R.id.screen1_toolbar);
+        setSupportActionBar(screen1Toolbar);
+
+}
 
             /**
              * This is the "top-level" fragment, showing a list of items that the
@@ -96,89 +90,3 @@ public class screen1 extends Activity {
         }
     }
 }
-
-/*
-import android.app.ListActivity;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-/**
- * Created by erinjensby on 3/26/17.
- */
-
-/*
-public class screen1 extends ListActivity {
-
-    private static final String TAG = "SourceList";
-
-    private ListView view;
-    private List<String> sources;
-    private ArrayAdapter<String> adapter;
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        createModel();
-
-        view = getListView();
-
-        setAdapter();
-
-        createOnItemClickListener();
-    }
-
-    private void setAdapter() {
-        // for layout that is simply a TextViews
-        adapter
-                = new ArrayAdapter<String>(
-                this,
-                R.layout.list_item, // android.R.layout.simple_list_item_1,
-                sources);
-
-/*
-        // for layout with TextView in more complex layout
-        adapter
-                = new ArrayAdapter<String>(
-                this, // context
-                R.layout.list_item, // layout of list items / rows
-                R.id.sourceTextView, // sub layout to place text
-                sources); // model of text
-
-        // don't comment out!
-        setListAdapter(adapter);
-    }
-
-    private void createModel() {
-        String[] rawData = getResources().getStringArray(R.array.sources);
-        sources = new ArrayList(Arrays.asList(rawData));
-    }
-
-    private void createOnItemClickListener() {
-
-        view.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
-            public void onItemClick(AdapterView<?> parent,
-                                    View v, int position, long id) {
-
-                Log.d(TAG, "Selected view: " + v);
-
-                String source = sources.get(position);
-
-                String toastString = "position: " + position +
-                        ", id: " + id + "\ndata: "
-                        + source;
-
-            }
-        });
-    }
-}
-*/
