604340f799697bd83fdd6cfc176c2e00b9187729
Erin Jensby <erinjensby@horatio-200.cs.utexas.edu>
2017-04-03 22:01:01 -0500

    added search and settings and menu

--- app/src/main/java/cs371m/myqueue/SettingsActivity.java
+++ app/src/main/java/cs371m/myqueue/SettingsActivity.java
@@ -1,9 +1,22 @@
 package cs371m.myqueue;
 
+import android.app.ListFragment;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -11,25 +24,86 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        getFragmentManager().beginTransaction().replace(android.R.id.content, new SettingsFragment()).commit();
+        setContentView(R.layout.settings_layout);
+
+        Toolbar settingsToolbar = (Toolbar)findViewById(R.id.settings_toolbar);
+        setSupportActionBar(settingsToolbar);
+
     }
 
-    public static class SettingsFragment extends PreferenceFragment {
-        private SharedPreferences sharedPrefs;
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_browse:
+                startActivity(new Intent(this, BrowseActivity.class));
+                return true;
+            case R.id.menu_bookmarks:
+                startActivity(new Intent(this, BrowseActivity.class));
+                return true;
+            case R.id.menu_search:
+                startActivity(new Intent(this, SearchActivity.class));
+                return true;
+            case R.id.menu_settings:
+                startActivity(new Intent(this, SettingsActivity.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+    /**
+     * This is the "top-level" fragment, showing a list of items that the
+     * user can pick.  Upon picking an item, it takes care of displaying the
+     * data to the user as appropriate based on the current UI layout.
+     */
+
+    public static class TitlesFragment extends ListFragment {
+
+        int mCurCheckPosition = 0;
 
         @Override
-        public void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
+        public void onActivityCreated(Bundle savedInstanceState) {
+            super.onActivityCreated(savedInstanceState);
 
-            getPreferenceManager().setSharedPreferencesName("AndroidMyQueue");
-            sharedPrefs = getActivity().getSharedPreferences("AndroidMyQueue", MODE_PRIVATE);
+            String[] rawData = getResources().getStringArray(R.array.settings_options);
+            List<String> sources = new ArrayList(Arrays.asList(rawData));
 
-            addPreferencesFromResource(R.xml.preferences);
+            // Populate list with our static array of titles.
+            setListAdapter(new ArrayAdapter<>(getActivity(),
+                    android.R.layout.simple_list_item_activated_1,
+                    sources));
 
+            if (savedInstanceState != null) {
+                // Restore last state for checked position.
+                mCurCheckPosition
+                        = savedInstanceState.getInt("curChoice", 0);
+            }
+
+            getListView().setChoiceMode(ListView.CHOICE_MODE_SINGLE);
         }
 
 
+        @Override
+        public void onSaveInstanceState(Bundle outState) {
+            super.onSaveInstanceState(outState);
+            outState.putInt("curChoice", mCurCheckPosition);
+        }
 
+        @Override
+        public void onListItemClick(ListView l, View v, int position, long id) {
+            addSource(position);
+        }
 
+        void addSource(int index) {
+            mCurCheckPosition = index;
+
+            // Not yet implemented, currently uses only Netflix automatically
+
+        }
     }
 }
